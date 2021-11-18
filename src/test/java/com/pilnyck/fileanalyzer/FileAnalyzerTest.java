bad6c653a83733f45d906f5bef65d5b784684b70
Vitaliu <pilnyck155@gmail.com>
2021-11-18 13:41:52 +0200

    Modifire FileAnalyzerTest class

--- src/test/java/com/pilnyck/fileanalyzer/FileAnalyzerTest.java
+++ src/test/java/com/pilnyck/fileanalyzer/FileAnalyzerTest.java
@@ -1,41 +1,7 @@
 package com.pilnyck.fileanalyzer;
-/*
-Используем классы FileInputStream, FileOutputStream, File
-Практика:
-1: Написать программу FileAnalyzer, которая в консоли принимает 2 параметра:
-1) путь к файлу
-2) слово
-Usage:
-java FileAnalyzer C:/test/story.txt duck
-
-Выводит:
-1) Кол-во вхождений искомого слова в файле
-2) Все предложения содержащие искомое слово(предложение заканчивается символами ".", "?", "!").
-Каждое предложение выводится с новой строки.
-
-
-2: Пишем class FileManager с методами
-public class FileManager {
-// public static int countFiles(String path) - принимает путь к папке,
-// возвращает количество файлов в папке и всех подпапках по пути
-public static int countFiles(String path) {
-return 0;
-}
-
-// public static int countDirs(String path) - принимает путь к папке,
-// возвращает количество папок в папке и всех подпапках по пути
-public static int countDirs(String path) {
-return 0;
-}
-}
-
-public static void copy(String from, String to) - метод по копированию папок и файлов.
-Параметр from - путь к файлу или папке, параметр to - путь к папке куда будет производиться копирование.
-public static void move(String from, String to) - метод по перемещению папок и файлов.
-Параметр from - путь к файлу или папке, параметр to - путь к папке куда будет производиться копирование.
- */
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -48,47 +14,54 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FileAnalyzerTest {
+    FileAnalyzer fileAnalyzer = new FileAnalyzer();
 
-//    @BeforeEach
-//    public void before(){
-//        FileAnalyzer fileAnalyzer = new FileAnalyzer();
-//    }
-
-
+    @DisplayName("test analyzer method")
     @Test
-    public void iTestCheckCorrectPath_AndDontThrowException_IfPathIsCorrect(){
+    public void testAnalyzerMethod() {
+        String path = "/home/vitaliu/IdeaProjects/IOStudy/src/test/resources/test4.txt";
+        String keyWord = "Dursley";
+
+        FileStatistics fileStatistics = fileAnalyzer.analyzer(path, keyWord);
+        int actualWordCount = fileStatistics.wordCount;
+        int expectedCount = 9;
+        List<String> actualSentencesWithKey = fileStatistics.getSentences();
+        assertEquals(expectedCount, actualWordCount);
+    }
+
+    @DisplayName("test correct path was entered")
+    @Test
+    public void iTestCheckCorrectPathMethod() {
         String path = "/home/vitaliu/IdeaProjects/IOStudy/src/test/resources/test_f.txt";
         File file = new File(path);
         boolean expected = file.exists();
         assertTrue(expected);
 
-        FileAnalyzer fileAnalyzer = new FileAnalyzer();
         fileAnalyzer.checkCorrectPath(path);
     }
 
+    @DisplayName("test incorrect path was entered and throw exception")
     @Test
-    public void iTestCheckCorrectPath_AndThrowException_IfFileNotExist(){
+    public void iTestCheckCorrectPath_AndThrowException_IfFileNotExist() {
         String wrongPath = "/home/vitaliu/IdeaProjects/IOStudy/src/test/test_f.txt";
         File file = new File(wrongPath);
         boolean expected = file.exists();
         assertFalse(expected);
 
-        FileAnalyzer fileAnalyzer = new FileAnalyzer();
         fileAnalyzer.checkCorrectPath(wrongPath);
     }
 
+    @DisplayName("test get content from file")
     @Test
-    public void testGetContent(){
-        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+    public void testGetContent() {
         String actual = fileAnalyzer.getContent("/home/vitaliu/IdeaProjects/IOStudy/src/test/resources/test_f.txt");
-        //String actual = ("/home/vitaliu/IdeaProjects/IOStudy/src/test/resources/test_f.txt");
         String expected = "London is a capital of Great Britain!";
         assertEquals(expected, actual);
     }
 
+    @DisplayName("test correct hack content to sentences")
     @Test
-    public void testHackToSentences(){
-        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+    public void testHackToSentences() {
         String content = "London is a capital of Great Britain! We all yong? Who is on duty today?";
         List<String> actual = fileAnalyzer.hackToSentences(content);
         List<String> expected = new LinkedList<>();
@@ -98,9 +71,9 @@ public class FileAnalyzerTest {
         assertEquals(expected, actual);
     }
 
+    @DisplayName("test find key word in List<String>")
     @Test
-    public void testFinder(){
-        FileAnalyzer fileAnalyzer = new FileAnalyzer();
+    public void testFinder() {
         List<String> linkedList = new LinkedList<>();
         linkedList.add("London is a capital of Great Britain");
         linkedList.add("We all yong");
